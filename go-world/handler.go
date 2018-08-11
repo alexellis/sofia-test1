e7f84cf3ad9df6dee9384a77916843f3042a87a0
Alex Ellis <alexellis2@gmail.com>
2018-08-11 20:23:05 +0100

    Update handler.go

--- go-world/handler.go
+++ go-world/handler.go
@@ -2,5 +2,5 @@ package function
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return "I'm a nano-service, in a serverless world"
+	return "I'm a nano-service, in a serverless world ftw"
 }
