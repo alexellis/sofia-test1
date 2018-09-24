f168430d3bb9edb46141938c78dc87a8752ef830
Alex Ellis <alexellis2@gmail.com>
2018-09-24 21:55:59 +0100

    Use Http_X_Hub_Signature
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- signed-and-sealed/handler.go
+++ signed-and-sealed/handler.go
@@ -8,7 +8,9 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	res := hmac.Validate(req, os.Getenv("Http_X_Github_Event"), os.Getenv("hmac_secret"))
+	xHubSignature := os.Getenv("Http_X_Hub_Signature")
+
+	res := hmac.Validate(req, xHubSignature, os.Getenv("hmac_secret"))
 
 	if res != nil {
 		return res.Error()
