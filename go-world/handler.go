184e8d0d115d2dd3c02790c689a5365d878f9f79
Alex Ellis <alexellis2@gmail.com>
2018-09-24 21:48:16 +0100

    Add HMAC example
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- go-world/handler.go
+++
@@ -1,6 +0,0 @@
-package function
-
-// Handle a serverless request
-func Handle(req []byte) string {
-	return "Go #ftw"
-}
---
+++ signed-and-sealed/handler.go
@@ -0,0 +1,18 @@
+package function
+
+import (
+	"os"
+
+	"github.com/alexellis/hmac"
+)
+
+// Handle a serverless request
+func Handle(req []byte) string {
+	res := hmac.Validate(req, os.Getenv("Http_X_GitHub_Event"), os.Getenv("hmac_secret"))
+
+	if res != nil {
+		return res.Error()
+	}
+
+	return "You got in - thanks for using the secret to sign your payload"
+}
